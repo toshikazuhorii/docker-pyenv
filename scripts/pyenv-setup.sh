4eb7ca96a875ccd380d531f17d11582c8bfb9adc
Toshikazu Horii <toshikazu-horii@m3.com>
2017-04-14 11:10:11 +0900

    :heavy_plus_sign: Add pyenv-virtualenv

--- scripts/pyenv-setup.sh
+++ scripts/pyenv-setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 git clone --depth 1 https://github.com/yyuu/pyenv.git ~/.pyenv && \
+git clone --depth 1 https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv && \
 rm -rfv /root/.pyenv/.git && \
+rm -rfv /root/.pyenv/plugins/pyenv-virtualenv/.git && \
 export PYENV_ROOT="/root/.pyenv" && \
 export PATH="$PYENV_ROOT/bin:$PATH" && \
 export CFLAGS='-O2' && \
